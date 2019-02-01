e397d7997cb3896c9777b42068237b847fc195af
rachitabansal <rachita.gemm30@gmail.com>
2019-01-31 22:12:09 -0500

    Updates

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/rachitabansal/WebstormProjects/portfolio/bansalrachita.github.io/my-portfolio/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
