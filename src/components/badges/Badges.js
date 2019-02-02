732c53e2426ab5fe7b75e03a3641173e9749b59a
rachitabansal <rachita.gemm30@gmail.com>
2019-02-01 21:01:37 -0500

    adding mobile friendly styles.

--- src/components/badges/Badges.js
+++ src/components/badges/Badges.js
@@ -1,11 +1,12 @@
 import React, {Component} from "react";
 import BadgesView from "./BadgesView";
+import "../../common/main.css";
 
 export default class Badges extends Component {
     render() {
         const {badges} = this.props;
         return (
-            <div>
+            <div className="badges-container">
                 <BadgesView badges={badges}/>
             </div>
         )
