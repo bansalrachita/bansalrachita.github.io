f5c072f434732f325eb2d9f8e7437f0442cdfbcf
rachitabansal <rachita.gemm30@gmail.com>
2019-02-01 21:04:43 -0500

    Updates

---
+++ pages-manifest-ff91ff7906e91fc397fe.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{135:function(e){e.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"}],dataPaths:{"dev-404-page-5f9":"920/path---dev-404-page-5-f-9-fab-NZuapzHg3X9TaN1iIixfv1W23E",index:"921/path---index-6a9-GmhnlB6N8wuGZMtY58KeeWUOnBk"}}}}]);
+//# sourceMappingURL=pages-manifest-ff91ff7906e91fc397fe.js.map
